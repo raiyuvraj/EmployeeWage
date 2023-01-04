6b5fe7511b4ebef47409d3f0da1330875425eac3
raiyuvraj <yuvirai017@gmail.com>
2023-01-04 05:51:01 -0800

    [Yuvraj]-Monthaly_Wage

--- EmployeeWage/Program.cs
+++ EmployeeWage/Program.cs
@@ -7,28 +7,34 @@ namespace EmployeeWage
         public const int Full_Time = 1;
         public const int Part_Time = 2;
         public const int Rate_Per_Hour = 20;
+        public const int Num_Of_Working_Days = 20;
         static void Main(string[] args)
         {
             int empHours = 0;
             int empWage = 0;
-
-
-            Random random= new Random();
-            int EmpCheck = random.Next(0,3);
-
-            switch (EmpCheck)
+            int totalWage = 0;
+            for (int day = 0; day < Num_Of_Working_Days; day++)
             {
-                case Full_Time:
-                    empHours = 8; break;
-                case Part_Time:
-                    empWage = 4; break;
-                default:
-                    empHours = 0;
-                        break;
-            }
+                Random random = new Random();
+                int EmpCheck = random.Next(0, 3);
 
-            empWage = empHours * Rate_Per_Hour;
-            Console.WriteLine("Daily Employee Wage :" +empWage);
+                switch (EmpCheck)
+                {
+                    case Full_Time:
+                        empHours = 8; break;
+                    case Part_Time:
+                        empWage = 4; break;
+                    default:
+                        empHours = 0;
+                        break;
+                }
+
+                empWage = empHours * Rate_Per_Hour;
+                totalWage += empWage;
+                Console.WriteLine("Daily Employee Wage: "+empWage);
+
+            }
+            Console.WriteLine("Employee Wage for months :" +totalWage);
         }
     }
 }
