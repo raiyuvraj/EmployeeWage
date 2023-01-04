376e8af55cdb232f8e5a62e671f4dae17adcf502
raiyuvraj <yuvirai017@gmail.com>
2023-01-04 04:47:28 -0800

    [Yuvraj]-Present/Absent

---
+++ EmployeeWage/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace EmployeeWage
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            int Full_Time = 1;
+            Random random= new Random();
+            int EmpCheck = random.Next(0,2);
+
+            if(EmpCheck == 1)
+            {
+                Console.WriteLine("Employee is Present");
+            }
+            else
+            {
+                Console.WriteLine("Employee is Absent");
+            }
+        }
+    }
+}
