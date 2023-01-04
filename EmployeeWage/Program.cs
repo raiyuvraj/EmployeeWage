86201695f3093a31cc3565e99959bafe0566939b
raiyuvraj <yuvirai017@gmail.com>
2023-01-04 05:07:02 -0800

    [Yuvraj]-PartTime

--- EmployeeWage/Program.cs
+++ EmployeeWage/Program.cs
@@ -5,16 +5,21 @@
         static void Main(string[] args)
         {
             int Full_Time = 1;
+            int Part_Time = 2;
             int Rate_Per_Hour = 20;
             int empHours = 0;
             int empWage = 0;
 
             Random random= new Random();
-            int EmpCheck = random.Next(0,2);
+            int EmpCheck = random.Next(0,3);
 
             if(EmpCheck == Full_Time)
             {
                 empHours = 8;
+            }
+            else if(EmpCheck == Part_Time)
+            {
+                empHours = 4;
             }
             else
             {
