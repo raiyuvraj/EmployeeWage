08c6125466851204d33b8974d6f375dfc075d543
raiyuvraj <yuvirai017@gmail.com>
2023-01-05 17:16:28 -0800

    [Yuvraj]-Woking_Hours_100

--- EmployeeWage/Program.cs
+++ EmployeeWage/Program.cs
@@ -1,40 +1,38 @@
-﻿using System.Reflection.Metadata;
-
-namespace EmployeeWage
+﻿class Program
 {
-    public class Program
+    public const int Full_Time = 1;
+    public const int Part_Time = 2;
+    public const int Wedge_Rate = 20;
+    public const int Working_Days = 20;
+    public const int Max_Working_Houors = 100;
+
+    static void Main(string[] args)
     {
-        public const int Full_Time = 1;
-        public const int Part_Time = 2;
-        public const int Rate_Per_Hour = 20;
-        public const int Num_Of_Working_Days = 20;
-        static void Main(string[] args)
+        int EmpHours = 0;
+        int Total_Hours = 0;
+        int Total_Days = 0;
+        while (Total_Hours <= Max_Working_Houors && Total_Days <= Working_Days) ;
         {
-            int empHours = 0;
-            int empWage = 0;
-            int totalWage = 0;
-            for (int day = 0; day < Num_Of_Working_Days; day++)
+            Total_Days++;
+            Random random = new Random();
+            int EmpCheck = random.Next(0, 3);
+            switch (EmpCheck)
             {
-                Random random = new Random();
-                int EmpCheck = random.Next(0, 3);
-
-                switch (EmpCheck)
-                {
-                    case Full_Time:
-                        empHours = 8; break;
-                    case Part_Time:
-                        empWage = 4; break;
-                    default:
-                        empHours = 0;
-                        break;
-                }
-
-                empWage = empHours * Rate_Per_Hour;
-                totalWage += empWage;
-                Console.WriteLine("Daily Employee Wage: "+empWage);
-
+                case Full_Time:
+                    EmpHours = 8;
+                    break;
+                case Part_Time:
+                    EmpHours = 4;
+                    break;
+                default:
+                    EmpHours = 0;
+                    break;
             }
-            Console.WriteLine("Employee Wage for months :" +totalWage);
+            Total_Hours += EmpHours;
+            Console.WriteLine("Total Days :" + Total_Days + "Total Hours: " + Total_Hours);
         }
+        int EmpWedge = Total_Hours * Wedge_Rate;
+        Console.WriteLine("Total Wedge :" + EmpWedge);
     }
+
 }
